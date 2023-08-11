495cc44755d1609a8647842bd028c5d70ba8752b
scivision <scivision@users.noreply.github.com>
2023-08-11 18:25:24 -0400

    move generator version to proper location

--- fake_cmd.cmake
+++ fake_cmd.cmake
@@ -8,6 +8,13 @@ endif()
 
 set(CMAKE_CXX_STANDARD 20)
 
+execute_process(COMMAND ${CMAKE_MAKE_PROGRAM} --version
+OUTPUT_VARIABLE gen_ver)
+string(REGEX REPLACE "\n" ";" gen_ver "${gen_ver}")
+list(GET gen_ver 0 gen_ver)
+message(STATUS "${CMAKE_GENERATOR} ${gen_ver}")
+
+
 configure_file(${CMAKE_CURRENT_LIST_DIR}/fake_cmd.cxx.in fake_cmd.cxx @ONLY)
 
 add_executable(fake_cmd fake_cmd.cxx)

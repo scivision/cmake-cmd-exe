8a8eae14abcc2ad1565766fd62bd14f996c17be4
scivision <scivision@users.noreply.github.com>
2023-08-11 15:25:41 -0400

    remove duplicate command

--- fake_cmd.cmake
+++ fake_cmd.cmake
@@ -9,3 +9,7 @@ set(CMAKE_CXX_STANDARD 20)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/fake_cmd.cxx.in fake_cmd.cxx @ONLY)
 
 add_executable(fake_cmd fake_cmd.cxx)
+
+add_test(NAME CopyFakeCmd
+COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:fake_cmd> ${CMAKE_CURRENT_BINARY_DIR}/cmd.exe)
+set_property(TEST CopyFakeCmd PROPERTY FIXTURES_SETUP copycmd)
